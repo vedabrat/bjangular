198464ee9e9c705f11d7731b0238851fee05b622
vedabrat <vedabrat>
2017-07-24 19:37:01 -0400

    chap5done

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,31 @@
+ // Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 399, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 257, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration:  204, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 226, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 190, audioUrl: 'assets/music/magenta' }  
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
