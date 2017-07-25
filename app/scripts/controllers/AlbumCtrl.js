f92ddbd09d4382d9f1ff510fa5323119080c617f
vedabrat <vedabrat>
2017-07-24 21:52:10 -0400

    chap5.0 partially done. issues with assignment number 3 bulletpoint 3 and 4

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,13 @@
+ (function() {
+     function albumData() {
+  	     this.albums = [];
+	     for (var i=0; i < 12; i++) {
+    	     this.albums.push(angular.copy(albumPicasso));
+     }
+
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
