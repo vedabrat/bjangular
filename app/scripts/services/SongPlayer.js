d8a1009f8483f6cd0dd1a77d3a2358d2aab2a3ab
vedabrat <vedabrat>
2017-08-01 16:31:21 -0400

    done with the assignment. i bet its not the best documentation

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,15 @@
  (function() {
+     function playSong() {
+        currentBuzzObject.play();
+        song.playing = true;
+     };
      function SongPlayer() {
          var SongPlayer = {};
          var currentSong = null;
-
-
  /**
  * @desc Buzz object audio file
  * @type {Object}
  */
-         
          var currentBuzzObject = null;
 /**
  * @function setSong
@@ -20,23 +21,19 @@
 		        currentBuzzObject.stop();
 		        currentSong.playing = null;
 		    }
-		 
 		    currentBuzzObject = new buzz.sound(song.audioUrl, {
 		        formats: ['mp3'],
 		        preload: true
 		    });
-		 
 		    currentSong = song;
 		 };
-
-
+/*@method play plays song
+@ desc if the current song is not the selected song, set song to the selected song, play the song, register it as playing. 
+if that isnt the case and the song is paused, play it after*/
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
          		 setSong(song);
-
-                 currentBuzzObject.play();
-                 song.playing = true;
-
+                 playSong();
              } else if (currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
                      currentBuzzObject.play();
@@ -50,5 +47,9 @@
 
          return SongPlayer;
      }
-     angular.module('blocJams').factory('SongPlayer', SongPlayer);
+
+     angular
+       .module('blocJams')
+       .factory('SongPlayer', SongPlayer);
  })();
+
