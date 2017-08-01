2a18f72867304438750850b967ff7858b8bef06a
vedabrat <vedabrat>
2017-08-01 16:11:53 -0400

    end of chapter seven

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,14 @@
  (function() {
-     function AlbumCtrl(Fixtures) {
+      function AlbumCtrl(Fixtures, SongPlayer) {
  	    this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
+
      }
 
      
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 
  })();
