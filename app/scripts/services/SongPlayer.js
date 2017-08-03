9f2f07e05b12d8d24eb40568f9c7c5e84baa3361
vedabrat <vedabrat>
2017-08-03 16:49:45 -0400

    BRADY IS THE MAN. HELPED ME FIX THE GAMEPLAN

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,58 +1,51 @@
  (function() {
-
      function SongPlayer() {
          var SongPlayer = {};
          var currentSong = null;
- /**
- * @desc Buzz object audio file
- * @type {Object}
- */
+         /**
+          * @desc Buzz object audio file
+          * @type {Object}
+          */
          var currentBuzzObject = null;
-
-/**
- * @function setSong
- * @desc Stops currently playing song and loads new audio file as currentBuzzObject
- * @param {Object} song
- */
-		 var setSong = function(song) {
-		    if (currentBuzzObject) {
-		        currentBuzzObject.stop();
-		        currentSong.playing = null;
-		    }
-		    currentBuzzObject = new buzz.sound(song.audioUrl, {
-		        formats: ['mp3'],
-		        preload: true
-		    });
-		    currentSong = song;
-		 };
-
-      function playSong() {
-        currentBuzzObject.play();
-        song.playing = true;
-     };
-/*@method play plays song
-@ desc if the current song is not the selected song, set song to the selected song, play the song, register it as playing. 
-if that isnt the case and the song is paused, play it after*/
+         /**
+          * @function setSong
+          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+          * @param {Object} song
+          */
+         var setSong = function(song) {
+             if (currentBuzzObject) {
+                 currentBuzzObject.stop();
+                 currentSong.playing = null;
+             }
+             currentBuzzObject = new buzz.sound(song.audioUrl, {
+                 formats: ['mp3'],
+                 preload: true
+             });
+             currentSong = song;
+         };
+         var playSong = function(song) {
+             song = currentSong;
+             currentBuzzObject.play();
+             song.playing = true;
+         };
+         /*@method play plays song
+         @ desc if the current song is not the selected song, set song to the selected song, play the song, register it as playing. 
+         if that isnt the case and the song is paused, play it after*/
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
-         		 setSong(song);
-                 playSong();
+                 setSong(song);
+                 playSong(song);
              } else if (currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
                      currentBuzzObject.play();
                  }
              }
          };
-          SongPlayer.pause = function(song) {
-		     currentBuzzObject.pause();
-		     song.playing = false;
-		  };
-
+         SongPlayer.pause = function(song) {
+             currentBuzzObject.pause();
+             song.playing = false;
+         };
          return SongPlayer;
      }
-
-     angular
-       .module('blocJams')
-       .factory('SongPlayer', SongPlayer);
+     angular.module('blocJams').factory('SongPlayer', SongPlayer);
  })();
-
