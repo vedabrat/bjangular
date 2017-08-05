13ee9f5b1961b252afbd237a984bc945eb89081a
vedabrat <vedabrat>
2017-08-05 14:38:46 -0400

    8 services . part 3 -- right before the Next and Previous Buttons

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,7 @@
  (function() {
      function SongPlayer() {
          var SongPlayer = {};
-         var currentSong = null;
+         SongPlayer.currentSong = null;
          /**
           * @desc Buzz object audio file
           * @type {Object}
@@ -15,16 +15,16 @@
          var setSong = function(song) {
              if (currentBuzzObject) {
                  currentBuzzObject.stop();
-                 currentSong.playing = null;
+                 SongPlayer.currentSong.playing = null;
              }
              currentBuzzObject = new buzz.sound(song.audioUrl, {
                  formats: ['mp3'],
                  preload: true
              });
-             currentSong = song;
+             SongPlayer.currentSong = song;
          };
          var playSong = function(song) {
-             song = currentSong;
+             song = SongPlayer.currentSong;
              currentBuzzObject.play();
              song.playing = true;
          };
@@ -32,16 +32,19 @@
          @ desc if the current song is not the selected song, set song to the selected song, play the song, register it as playing. 
          if that isnt the case and the song is paused, play it after*/
          SongPlayer.play = function(song) {
-             if (currentSong !== song) {
+             song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
                  setSong(song);
                  playSong(song);
-             } else if (currentSong === song) {
+             } else if (SongPlayer.currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
                      currentBuzzObject.play();
                  }
              }
          };
          SongPlayer.pause = function(song) {
+             song = song || SongPlayer.currentSong;
+
              currentBuzzObject.pause();
              song.playing = false;
          };
